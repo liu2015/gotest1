e6066cea6102f8b7bc0d1072a7c0a3fbaf431b2a
liu2015 <176784830@qq.com>
2024-03-13 11:15:42 +0800

    dijaio

--- chantest/chantest.go
+++ chantest/chantest.go
@@ -7,5 +7,10 @@ func Chantest() {
 
 	mych = make(chan int, 4)
 	fmt.Println("长度~~：", len(mych), "容量", cap(mych))
+	mych <- 124
+	fmt.Println("写入管道", len(mych), "容量", cap(mych))
+
+	num := <-mych
+	fmt.Println("读取数据", num)
 
 }
