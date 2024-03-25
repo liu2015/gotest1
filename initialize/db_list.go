9de9b413bf1d58112ff56176075dd28deceb0f56
liu2015 <176784830@qq.com>
2024-03-25 18:31:35 +0800

    dijiao

--- initialize/db_list.go
+++ initialize/db_list.go
@@ -1,3 +1,22 @@
 package initialize
 
+import (
+	"ginserver/global"
+
+	"gorm.io/gorm"
+)
+
 const sys = "system"
+
+func DBList() {
+	dbMap := make(map[string]*gorm.DB)
+	for _, info := range global.GVA_CONFIG.DBList {
+		if info.Disable {
+			continue
+		}
+		switch info.Type {
+		case "mysql":
+			dbMap[info.AliasName] = gormm
+		}
+	}
+}
---
+++ initialize/gorm_mysql.go
@@ -0,0 +1,53 @@
+package initialize
+
+import (
+	"ginserver/config"
+	"ginserver/global"
+	"ginserver/initialize/internal"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// 初始化mysql
+func GormMysql() *gorm.DB {
+	m := global.GVA_CONFIG.Mysql
+	if m.Dbname == "" {
+		return nil
+	}
+	mysqlConfig := mysql.Config{
+		DSN:                       m.Dsn(),
+		DefaultStringSize:         191,
+		SkipInitializeWithVersion: false,
+	}
+	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config(m.Prefix, m.Singular)); err != nil {
+		return nil
+	} else {
+		db.InstanceSet("gorm:table_options", "ENGINE="+m.Engine)
+		sqlDB, _ := db.DB()
+		sqlDB.SetMaxIdleConns(m.MaxIdleConns)
+		sqlDB.SetMaxOpenConns(m.MaxIdleConns)
+		return db
+	}
+
+}
+
+func GormMysqlByConfig(m config.Mysql) *gorm.DB {
+	if m.Dbname == "" {
+		return nil
+	}
+	mysqlConfig := mysql.Config{
+		DSN:                       m.Dsn(),
+		DefaultStringSize:         191,
+		SkipInitializeWithVersion: false,
+	}
+	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config(m.Prefix, m.Singular)); err != nil {
+		panic(err)
+	} else {
+		db.InstanceSet("gorm:table_options", "ENGINE=InnoDB")
+		sqlDB, _ := db.DB()
+		sqlDB.SetMaxIdleConns(m.MaxIdleConns)
+		sqlDB.SetMaxOpenConns(m.MaxIdleConns)
+		return db
+	}
+}
